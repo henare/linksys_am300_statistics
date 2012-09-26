75caf18e5deb3b8a22a1a3f09529e74aeaeac636
Henare Degan <henare.degan@gmail.com>
2012-09-26 13:21:15 +1000

    Allow status to be refreshed

--- dslmodem.rb
+++ dslmodem.rb
@@ -3,21 +3,26 @@ require 'mechanize'
 
 class DSLModem
   def initialize(url='http://192.168.1.1/index.stm?title=Status-Modem', username, password)
+    @url, @username, @password = url, username, password
+    get_stats_page
+  end
+
+  def get_stats_page
     # Get the login page
     agent = Mechanize.new
-    @page = agent.get(url)
+    @page = agent.get(@url)
 
     # If we're already logged in (don't ask), then we already have the page we need
     return @page if @page.title != "Log In"
 
     # Login
     form = @page.forms.first
-    form.field_with(:name => "username").value = username
-    form.field_with(:name => "password").value = password
+    form.field_with(:name => "username").value = @username
+    form.field_with(:name => "password").value = @password
     form.submit
 
     # Get stats page
-    @page = agent.get(url)
+    @page = agent.get(@url)
   end
 
   def statistics
